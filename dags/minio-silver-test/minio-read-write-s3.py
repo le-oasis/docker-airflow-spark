493b7f065bd73df77170bd48a69901eeb4780f3e
Mahmud Oyinloye <64417495+le-oasis@users.noreply.github.com>
2022-08-25 09:44:35 +0100

    Minio-Spark-Airflow-DAG

--- dags/minio-silver-test/minio-read-write-s3.py
+++ dags/minio-silver-test/minio-read-write-s3.py
@@ -1,10 +1,12 @@
-# Step 1: Importing modules
-# Import Python dependencies needed for the workflow
+###############################################
+# Import Necessary Libraries
 import airflow
 from datetime import timedelta
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 ###############################################
 # Parameters & Arguments
@@ -16,17 +18,27 @@ minio_conf = {
     "spark.hadoop.fs.s3a.secret.key": "minio123",
     "spark.hadoop.fs.s3a.path.style.access": "true",
     "spark.hadoop.fs.s3a.connection.ssl.enabled": "false",
+    "spark.sql.catalogImplementation": "hive",
+    "spark.sql.hive.metastore.version": "2.3.9",
+    "spark.sql.hive.metastore.jars": "builtin",
+    "spark.sql.hive.metastore.sharedPrefixes": "org.mariadb.jdbc,com.mysql.cj.jdbc,com.mysql.jdbc,org.postgresql,com.microsoft.sqlserver,oracle.jdbc",
+    "spark.sql.hive.metastore.schema.verification": "true",
+    "spark.sql.hive.metastore.schema.verification.record.version": "true",
+    "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+    "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog"
 }
+
+
 ###############################################
 # Spark App Name; shown on Spark UI
-spark_app_name="Minio_Spark"
+spark_app_name = "Minio-Spark"
 ###############################################
 # Path to Jars
 jar_home="/usr/local/spark/resources"
 ###############################################
 # Runtime Arguments
-app_jars=f'{jar_home}/jars/hadoop-aws-3.2.0.jar,{jar_home}/jars/hadoop-cloud-storage-3.2.0.jar,{jar_home}/jars/aws-java-sdk-bundle-1.11.375.jar'
-driver_class_path=f'{jar_home}/jars/hadoop-aws-3.2.0.jar:{jar_home}/jars/hadoop-cloud-storage-3.2.0.jar:{jar_home}/jars/aws-java-sdk-bundle-1.11.375.jar'
+app_jars=f'{jar_home}/jars/hadoop-cloud-storage-3.2.0.jar,{jar_home}/jars/aws-java-sdk-bundle-1.11.375.jar'
+driver_class_path=f'{jar_home}/jars/hadoop-cloud-storage-3.2.0.jar:{jar_home}/jars/aws-java-sdk-bundle-1.11.375.jar'
 ###############################################
 # DAG Definition
 ###############################################
@@ -49,14 +61,15 @@ with DAG(
 )
 ###############################################
 # Sequence of Tasks
-Pull_Data_From_S3 = SparkSubmitOperator(task_id='Pull_Data',
-                                              conn_id='spark_connect',
-                                              application='/usr/local/spark/app/minio/minio.py',
+###############################################
+Silver_Customers = SparkSubmitOperator(task_id='Silver_Customer_Transform',
+                                              conn_id='mcstore_con_spark',
+                                              application='/usr/local/spark/app/minio-test/silver-customers.py',
                                               conf=minio_conf,
                                               total_executor_cores=2,
                                               jars=app_jars,
                                               driver_class_path=driver_class_path,
-                                              packages="org.apache.spark:spark-hadoop-cloud_2.13:3.3.0",
+                                              packages="io.delta:delta-core_2.12:2.0.0,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.spark:spark-hadoop-cloud_2.13:3.2.0",
                                               executor_cores=2,
                                               executor_memory='5g',
                                               driver_memory='5g',
@@ -64,8 +77,39 @@ Pull_Data_From_S3 = SparkSubmitOperator(task_id='Pull_Data',
                                               execution_timeout=timedelta(minutes=10),
                                               dag=dag
                                               )                                             
-
+###############################################
+Silver_Orders = SparkSubmitOperator(task_id='Silver_Orders_Transform',
+                                              conn_id='mcstore_con_spark',
+                                              application='/usr/local/spark/app/minio-test/silver-orders.py',
+                                              conf=minio_conf,
+                                              total_executor_cores=2,
+                                              jars=app_jars,
+                                              driver_class_path=driver_class_path,
+                                              packages="io.delta:delta-core_2.12:2.0.0,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.spark:spark-hadoop-cloud_2.13:3.2.0",
+                                              executor_cores=2,
+                                              executor_memory='5g',
+                                              driver_memory='5g',
+                                              name=spark_app_name,
+                                              execution_timeout=timedelta(minutes=10),
+                                              dag=dag
+                                              )                                             
+###############################################
+Silver_Products = SparkSubmitOperator(task_id='Silver_Product_Transform',
+                                              conn_id='mcstore_con_spark',
+                                              application='/usr/local/spark/app/minio-test/silver-products.py',
+                                              conf=minio_conf,
+                                              total_executor_cores=2,
+                                              jars=app_jars,
+                                              driver_class_path=driver_class_path,
+                                              packages="io.delta:delta-core_2.12:2.0.0,org.apache.hadoop:hadoop-aws:3.2.0,org.apache.spark:spark-hadoop-cloud_2.13:3.2.0",
+                                              executor_cores=2,
+                                              executor_memory='5g',
+                                              driver_memory='5g',
+                                              name=spark_app_name,
+                                              execution_timeout=timedelta(minutes=10),
+                                              dag=dag
+                                              )                                             
 ###############################################
 end_task = DummyOperator(task_id='end_task')                                  
 ###############################################
-start_task >> Pull_Data_From_S3 >> end_task
+start_task >> Silver_Customers  >> Silver_Orders >> Silver_Products >> end_task
