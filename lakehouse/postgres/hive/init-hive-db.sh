f3e355894d98986a0e7c0744123193db22661718
Mahmud Oyinloye <64417495+le-oasis@users.noreply.github.com>
2024-04-01 15:08:48 +0100

    Refractor code

--- lakehouse/postgres/hive/init-hive-db.sh
+++ lakehouse/postgres/hive/init-hive-db.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -e
 
+# Use environment variables for user and password
+: "${HIVE_USER:=hive}"
+: "${HIVE_PASSWORD:=hive}"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE USER hive WITH PASSWORD 'hive';
-  CREATE DATABASE metastore;
-  GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
+  DO \$\$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$HIVE_USER') THEN
+      CREATE USER $HIVE_USER WITH PASSWORD '$HIVE_PASSWORD';
+    END IF;
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'metastore') THEN
+      CREATE DATABASE metastore;
+    END IF;
+  END
+  \$\$;
+
+  GRANT ALL PRIVILEGES ON DATABASE metastore TO $HIVE_USER;
 
   \c metastore
 
@@ -15,7 +28,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 
   \pset tuples_only
   \o /tmp/grant-privs
-SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO hive ;'
+SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO $HIVE_USER ;'
 FROM pg_tables
 WHERE tableowner = CURRENT_USER and schemaname = 'public';
   \o
