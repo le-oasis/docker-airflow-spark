3c8f14951fc8d49b0df351e79b24d8123f1794d0
Mahmud Oyinloye <oyinlola14@gmail.com>
2024-05-11 09:59:35 +0000

    postgres,hive,fix

--- postgres/create_ranger_users.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER ranger WITH PASSWORD 'security';
-CREATE USER admin WITH PASSWORD 'security';
-CREATE DATABASE ranger;
-GRANT ALL PRIVILEGES ON DATABASE ranger to admin;
--- postgres/hive/init-hive-db.sh
+++ postgres/hive/init-hive-db.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE USER hive WITH PASSWORD 'hive';
-  CREATE DATABASE metastore;
-  GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
+# Variables
+DB_USER="hive"
+DB_NAME="metastore"
+SCHEMA_NAME="public"
+RANGER_USER="ranger"
+RANGER_PASSWORD="security"
+ADMIN_USER="admin"
+ADMIN_PASSWORD="security"
+RANGER_DB="ranger"
 
-  \c metastore
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  -- Create user if it doesn't exist
+  DO \$\$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$DB_USER') THEN
+      CREATE USER $DB_USER WITH PASSWORD '$DB_USER';
+    END IF;
+  END
+  \$\$;
+
+  -- Create database if it doesn't exist
+  DO \$\$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME') THEN
+      CREATE DATABASE $DB_NAME;
+    END IF;
+  END
+  \$\$;
+
+  GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+
+  \c $DB_NAME
 
   \i /var/lib/postgresql/hive/hive-schema-2.3.0.postgres.sql
   \i /var/lib/postgresql/hive/hive-txn-schema-2.3.0.postgres.sql
@@ -15,9 +41,15 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 
   \pset tuples_only
   \o /tmp/grant-privs
-SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO hive ;'
-FROM pg_tables
-WHERE tableowner = CURRENT_USER and schemaname = 'public';
+  SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO $DB_USER ;'
+  FROM pg_tables
+  WHERE tableowner = CURRENT_USER and schemaname = '$SCHEMA_NAME';
   \o
   \i /tmp/grant-privs
+
+  -- Create ranger users
+  CREATE USER $RANGER_USER WITH PASSWORD '$RANGER_PASSWORD';
+  CREATE USER $ADMIN_USER WITH PASSWORD '$ADMIN_PASSWORD';
+  CREATE DATABASE $RANGER_DB;
+  GRANT ALL PRIVILEGES ON DATABASE $RANGER_DB to $ADMIN_USER;
 EOSQL
